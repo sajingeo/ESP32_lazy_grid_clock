ca11a77ac630ec3e4cb1b6566879a3a82eda3b7f
Sajin George <sajingeo@amazon.com>
2023-04-04 13:29:46 -0400

    added cool visuals

--- clock/clock.ino
+++ clock/clock.ino
@@ -169,9 +169,134 @@ void setup()
 
 }
 
+// List of patterns to cycle through.  Each is defined as a separate function below.
+typedef void (*SimplePatternList[])();
+SimplePatternList gPatterns = { rainbow, rainbowWithGlitter, confetti, sinelon, juggle, bpm };
+
+uint8_t gCurrentPatternNumber = 0; // Index number of which pattern is current
+uint8_t gHue = 0; // rotating "base color" used by many of the patterns
+
+void demo()
+{
+  while(1)
+  {
+  // Call the current pattern function once, updating the 'leds' array
+  gPatterns[gCurrentPatternNumber]();
+
+  // send the 'leds' array out to the actual LED strip
+  FastLED.show();  
+  // insert a delay to keep the framerate modest
+  FastLED.delay(1000/FRAMES_PER_SECOND); 
+
+  // do some periodic updates
+  EVERY_N_MILLISECONDS( 20 ) { gHue++; } // slowly cycle the "base color" through the rainbow
+  EVERY_N_SECONDS( 10 ) { nextPattern(); } // change patterns periodically
+
+  if(Serial.available())
+    {
+      String command = Serial.readStringUntil('\n');
+      if(command == "q")
+      {
+        break;
+      }
+    }
+  }
+}
+
+#define ARRAY_SIZE(A) (sizeof(A) / sizeof((A)[0]))
+
+void nextPattern()
+{
+  // add one to the current pattern number, and wrap around at the end
+  gCurrentPatternNumber = (gCurrentPatternNumber + 1) % ARRAY_SIZE( gPatterns);
+}
+
+void rainbow() 
+{
+  // FastLED's built-in rainbow generator
+  fill_rainbow( leds, LED_COUNT, gHue, 7);
+}
+
+void rainbowWithGlitter() 
+{
+  // built-in FastLED rainbow, plus some random sparkly glitter
+  rainbow();
+  addGlitter(80);
+}
+
+void addGlitter( fract8 chanceOfGlitter) 
+{
+  if( random8() < chanceOfGlitter) {
+    leds[ random16(LED_COUNT) ] += CRGB::White;
+  }
+}
+
+void confetti() 
+{
+  // random colored speckles that blink in and fade smoothly
+  fadeToBlackBy( leds, LED_COUNT, 10);
+  int pos = random16(LED_COUNT);
+  leds[pos] += CHSV( gHue + random8(64), 200, 255);
+}
+
+void sinelon()
+{
+  // a colored dot sweeping back and forth, with fading trails
+  fadeToBlackBy( leds, LED_COUNT, 20);
+  int pos = beatsin16( 13, 0, LED_COUNT-1 );
+  leds[pos] += CHSV( gHue, 255, 192);
+}
+
+void bpm()
+{
+  // colored stripes pulsing at a defined Beats-Per-Minute (BPM)
+  uint8_t BeatsPerMinute = 62;
+  CRGBPalette16 palette = PartyColors_p;
+  uint8_t beat = beatsin8( BeatsPerMinute, 64, 255);
+  for( int i = 0; i < LED_COUNT; i++) { //9948
+    leds[i] = ColorFromPalette(palette, gHue+(i*2), beat-gHue+(i*10));
+  }
+}
+
+void juggle() {
+  // eight colored dots, weaving in and out of sync with each other
+  fadeToBlackBy( leds, LED_COUNT, 20);
+  uint8_t dothue = 0;
+  for( int i = 0; i < 8; i++) {
+    leds[beatsin16( i+7, 0, LED_COUNT-1 )] |= CHSV(dothue, 200, 255);
+    dothue += 32;
+  }
+}
+
 
 void loop()
 {
+      if(Serial.available())
+    {
+      String command = Serial.readStringUntil('\n');
+      if(command == "p")
+      {
+        paletteSwitcher();
+      }
+      if(command == "d")
+      {
+        displayModeSwitcher();
+      }
+      if(command == "b")
+      {
+        brightnessSwitcher();
+      }
+      if(command == "c")
+      {
+        colorModeSwitcher();
+      }
+      if(command == "x")
+      {
+        demo();
+      }
+    }
+
+
     static uint8_t lastSecondDisplayed = 0;           // This keeps track of the last second when the display was updated (HH:MM and HH:MM:SS)
     static unsigned long lastCheckRTC = millis();     // This will be used to read system time in case no RTC is defined (not supported!)
     static bool doUpdate = false;                     // Update led content whenever something sets this to true. Coloring will always happen at fixed intervals!
@@ -194,27 +319,6 @@ void loop()
 
     syncHelper();
 
-    }
-
-    if(Serial.available())
-    {
-      String command = Serial.readStringUntil('\n');
-      if(command == "p")
-      {
-        paletteSwitcher();
-      }
-      if(command == "d")
-      {
-        displayModeSwitcher();
-      }
-      if(command == "b")
-      {
-        brightnessSwitcher();
-      }
-      if(command == "c")
-      {
-        colorModeSwitcher();
-      }
     }
 
     colorizeOutput(colorMode);                           // 1C, 2C, 3C...colorize the data inside the led array right now...
